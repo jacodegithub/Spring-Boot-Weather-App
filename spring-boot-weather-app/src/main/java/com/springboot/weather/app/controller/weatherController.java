11c582730a4e3b56d7e9a5be215cf1a1a6e2f968
jacodegithub <dudeobama246@gmail.com>
2023-09-03 23:47:41 +0530

    commiting backend of weather-ap

--- spring-boot-weather-app/src/main/java/com/springboot/weather/app/controller/weatherController.java
+++ spring-boot-weather-app/src/main/java/com/springboot/weather/app/controller/weatherController.java
@@ -35,85 +35,11 @@ public class weatherController {
         try {
             wService.setUnit(unit);
             WeatherResponse object = wService.getWeather(cityName);
+            System.out.println("object -> "+object);
             return new ResponseEntity<>(object, HttpStatus.OK);
         }catch (JSONException e) {
             e.printStackTrace();
             return null;
         }
     }
-
-//    @GetMapping("/{city}/weather")
-//    public ResponseEntity<List<Weather>> getWeatherArray(@PathVariable String city) {
-//        List<Weather> array = wService.getWeatherArray(city);
-//        return new ResponseEntity<>(array, HttpStatus.OK);
-//    }
-//
-//    @GetMapping("/{unit}/{cityName}/coord")
-//    public ResponseEntity<Coord> getCoordinates(@PathVariable String unit,
-//                                                @PathVariable String cityName) {
-//        wService.setUnit(unit);
-//        Coord coord = wService.getCoord(cityName);
-//        return new ResponseEntity<>(coord, HttpStatus.OK);
-//    }
-//
-//    @GetMapping("/{city}/currentTime")
-//    public ResponseEntity<Date> getDateTime(@PathVariable String city) {
-//        Long dateTime = wService.getDateTime(city);
-//        Long milliseconds = dateTime * 1000L;
-//        Date date = new Date(milliseconds);
-//        logger.info("date : {}",date);
-//        return new ResponseEntity<>(date, HttpStatus.OK);
-//    }
-//
-//    @GetMapping("/{city}/temperature")
-//    public ResponseEntity<Float> getTemperature(@PathVariable String city,
-//                                                @RequestParam(defaultValue = "metric") String unit) {
-//        wService.setUnit(unit);
-//        Float temperature = wService.getTemperature(city);
-//        return new ResponseEntity<>(temperature, HttpStatus.OK);
-//    }
-//
-//    @GetMapping("/{city}/feels_like")
-//    public ResponseEntity<Integer> getFeelsLike(@PathVariable String city,
-//                                                @RequestParam(defaultValue = "metric") String unit) {
-//        wService.setUnit(unit);
-//        Integer feels_like = wService.getFeelsLike(city);
-//        return new ResponseEntity<>(feels_like, HttpStatus.OK);
-//    }
-//
-//    @GetMapping("/{city}/wind")
-//    public ResponseEntity<Float> getWindSpeed(@PathVariable String city) {
-//        Float windSpeed = wService.getWindSpeed(city);
-//        return new ResponseEntity<>(windSpeed, HttpStatus.OK);
-//    }
-//
-//    @GetMapping("/{city}/humidity")
-//    public ResponseEntity<Float> getWeatherHumidity(@PathVariable String city) {
-//        Float humidity = wService.getHumidity(city);
-//        return new ResponseEntity<>(humidity, HttpStatus.OK);
-//    }
-//
-//    @GetMapping("/{city}/visibility")
-//    public ResponseEntity<Integer> getWeatherVisibility(@PathVariable String city) {
-//        int visibility = wService.getVisibility(city);
-//        return new ResponseEntity<>(visibility, HttpStatus.OK);
-//    }
-//
-//    @GetMapping("/{city}/pressure")
-//    public ResponseEntity<Float> getWeatherPressure(@PathVariable String city) {
-//        Float pressure = wService.gerPressure(city);
-//        return new ResponseEntity<>(pressure, HttpStatus.OK);
-//    }
-//
-//    @GetMapping("/{city}/grnd")
-//    public ResponseEntity<Integer> getWeatherGrndlvl(@PathVariable String city) {
-//        int windSpeed = wService.getGrntlvl(city);
-//        return new ResponseEntity<>(windSpeed, HttpStatus.OK);
-//    }
-//
-//    @GetMapping("/{city}/sea")
-//    public ResponseEntity<Integer> getWeatherSealvl(@PathVariable String city) {
-//        int windSpeed = wService.getSealvl(city);
-//        return new ResponseEntity<>(windSpeed, HttpStatus.OK);
-//    }
 }
--- spring-boot-weather-app/src/test/java/com/springboot/weather/app/SpringBootWeatherAppApplicationTests.java
+++ spring-boot-weather-app/src/test/java/com/springboot/weather/app/SpringBootWeatherAppApplicationTests.java
@@ -3,12 +3,37 @@ package com.springboot.weather.app;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.google.gson.Gson;
+import com.springboot.weather.app.dto.WeatherResponse;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 @SpringBootTest
 class SpringBootWeatherAppApplicationTests {
 
 	@Test
 	void contextLoads() {
+		String APIurl = "https://api.openweathermap.org/data/2.5/weather?q=";
+        String url = APIurl+"koppal"+"&units="+"metric"+"&appid="+"3b7a61b69fcba80c3efdeb434adb36d6";
 
+        // logger.info("url : {}", url);
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        try(Response response = client.newCall(request).execute()) {
+            String jsonResponse = response.body().string();
+            Gson gson = new Gson();
+            WeatherResponse wResponse = gson.fromJson(jsonResponse, WeatherResponse.class);
+            // logger.info("converted file : {}", wResponse);
+            // logger.info("jsonResponse: {}", jsonResponse);
+            System.out.println("response ->"+wResponse);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
 	}
 
 }
